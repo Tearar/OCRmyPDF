b867bcd615dfe1cab84f26ef2d5350af04d82fd6
James R. Barlow <jim@purplerock.ca>
2018-06-29 14:09:04 -0700

    ruffus again

--- setup.py
+++ setup.py
@@ -255,7 +255,7 @@ setup(
                                   # block 5.1.0, broken wheels
         'python-xmp-toolkit >= 2, < 3',
         'reportlab >= 3.3.0',     # oldest released version with sane image handling
-        'ruffus == 2.6.3',        # pinned - ocrmypdf implements a 2.6.3 workaround
+        'ruffus >= 2.6.3',        # pinned - ocrmypdf implements a 2.6.3 workaround
     ],
     extras_require={
         'fitz': [],  # Backward compatibility
